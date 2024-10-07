ac6a1bb69a063cbad9259c12e5e1bdf09739a24d
yu3nunu <yuki.n.19960405@gmail.com>
2024-10-07 15:19:41 +0900

    2-15

---
+++ Study/chapter2/src/chapter2/Ex1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("Hello Java");
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
