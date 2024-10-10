223850c9979f29001f570e137ae19aafaac9999c
yu3nunu <yuki.n.19960405@gmail.com>
2024-10-10 15:12:07 +0900

    Update Task2_22.java

--- Study/2-22/src/study/Task2_22.java
+++ Study/2-22/src/study/Task2_22.java
@@ -27,23 +27,20 @@ public class Task2_22 {
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述します。
-        // Calendar.MONTHは0から11の範囲で月を表します。
-        // そのため、ユーザーが1月を1と考えて加算すると、
-        // 実際には0から始まるため想定外の値となる可能性があります。
+        // [Calendar.MONTHは0から11の範囲で月を表すため、1月を1とすると実際には0から始まるため想定していない値となる]
 
-        System.out.println(calendar.get(Calendar.YEAR)); // 年を表示
-        System.out.println(calendar.get(Calendar.MONTH) + 1); // 月を表示 (1から12の範囲に調整)
-        System.out.println(calendar.get(Calendar.DATE)); // 日を表示
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.DATE));
 
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
-        calendar.add(Calendar.YEAR, 1);   // 1年加算
-        calendar.add(Calendar.MONTH, 2);   // 2ヶ月加算
-        calendar.add(Calendar.DATE, 15);    // 15日加算
+        calendar.add(Calendar.YEAR, 1);
+        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.DATE, 15);
 
-        // 日付を表示
-        System.out.println(calendar.get(Calendar.YEAR)); // 新しい年
-        System.out.println(calendar.get(Calendar.MONTH) + 1); // 新しい月
-        System.out.println(calendar.get(Calendar.DATE)); // 新しい日
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.DATE));
 
         // うるう年判定
         printLeapYear(calendar.get(Calendar.YEAR));
